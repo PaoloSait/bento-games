90ae32fc5b0ab91edd2a5d734c69f44986cf04b3
PaoloSait <paolosait7@gmail.com>
2024-10-02 17:36:02 +0200

    Added gisnep and reorderd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,7 @@
             <GameWidget
                 color={"bg-strands"}
                 href={"https://www.nytimes.com/games/strands"}
-                imgRef={"https://www.nytimes.com/games/prototype/dist/strands100px.ea3b90d2.svg"}
+                imgRef={"https://www.nytimes.com/games-assets/v2/metadata/strands-favicon.ico?v=v2410021032"}
                 gameName={"Strands"}
                 frequency={"Daily"}
                 logo={"https://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png"}
@@ -68,13 +68,15 @@
                 nameColor={"text-white"}
             ></GameWidget>
             <GameWidget
-                color={"bg-[#eef510]"}
-                href={"https://popidle.the-sound.co.uk/guess-the-album"}
-                imgRef={"/popidle_logo.png"}
-                gameName={"Popidle"}
+                color={"bg-[#d6e8ff]"}
+                href={"https://www.minutecryptic.com/"}
+                imgRef={"https://www.minutecryptic.com/icon.ico?73f70e8b5bf80676"}
+                gameName={"Minute Cryptic"}
                 frequency={"Daily"}
+                nameColor={"text-black"}
                 inverseTag={true}
             ></GameWidget>
+
             <GameWidget
                 color={"bg-[#31302e]"}
                 href={"https://travle.earth"}
@@ -100,6 +102,15 @@
                 nameColor={"text-white"}
             ></GameWidget>
             <GameWidget
+                color={"bg-[#f2f2f2]"}
+                href={"https://gisnep.com/"}
+                imgRef={"https://gisnep.com/safari-pinned-tab.svg"}
+                gameName={"Gisnep"}
+                frequency={"Daily"}
+                nameColor={"text-black"}
+                inverseTag={true}
+            ></GameWidget>
+            <GameWidget
                 color={"bg-[#ff7070]"}
                 href={"https://www.techyonic.co/swiftle"}
                 imgRef={"https://www.techyonic.co/favicon.ico"}
@@ -108,18 +119,15 @@
                 nameColor={"text-black"}
                 inverseTag={true}
             ></GameWidget>
+
             <GameWidget
-                color={"bg-[#d6e8ff]"}
-                href={"https://www.minutecryptic.com/"}
-                imgRef={"https://www.minutecryptic.com/icon.ico?73f70e8b5bf80676"}
-                gameName={"Minute Cryptic"}
+                color={"bg-[#eef510]"}
+                href={"https://popidle.the-sound.co.uk/guess-the-album"}
+                imgRef={"/popidle_logo.png"}
+                gameName={"Popidle"}
                 frequency={"Daily"}
-                nameColor={"text-black"}
                 inverseTag={true}
             ></GameWidget>
-
-            
-
         </div>
     </div>
 </body>
